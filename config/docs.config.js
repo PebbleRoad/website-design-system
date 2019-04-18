410efdc51a6aab32dcb2497ea47493d222554561
Hui Min <huimin@pebbleroad.com>
2019-04-18 14:39:04 +0800

    tokens: update base fonts

--- config/docs.config.js
+++ config/docs.config.js
@@ -17,7 +17,7 @@ module.exports = {
     maxWidth: "100%",
     sidebarWidth: 240,
     fontFamily: {
-      base: ["'Fira Sans'", "Helvetica", "Arial", "sans-serif"],
+      base: ["'Lato'", "Helvetica", "Arial", "sans-serif"],
       monospace: ["Consolas", "'Liberation Mono'", "Menlo", "monospace"],
     },
   },
