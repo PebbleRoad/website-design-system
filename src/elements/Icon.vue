43783fef8862a4bb263e461ff2d2e04c35cbb933
Hui Min <huimin@pebbleroad.com>
2019-05-02 15:04:10 +0800

    fix(Icon): add vertical-align css to element

--- src/elements/Icon.vue
+++ src/elements/Icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="type" :aria-label="ariaLabel" :class="['icon', size]" v-html="svg"/>
+  <component :is="type" :aria-label="ariaLabel" :class="['icon', size]" v-html="svg" />
 </template>
 
 <script>
@@ -71,6 +71,8 @@ export default {
 // We don’t want to use scoped since these styles need to cascade down to SVGs.
 // We also want to be able to style .icon inside buttons etc.
 .icon {
+  vertical-align: middle;
+
   @include reset;
   &.large svg {
     width: $space-l;
