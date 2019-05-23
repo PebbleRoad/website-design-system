078bd2aae4f1cd63e45db07d8a2309684837a2fa
Hui Min <huimin@pebbleroad.com>
2019-05-23 16:40:05 +0800

    feat(Heading): add marginBottom prop

--- src/elements/Heading.vue
+++ src/elements/Heading.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="level" class="heading">
+  <component :is="level" :class="['heading', { margin__bottom: marginBottom === true }]">
     <slot />
   </component>
 </template>
@@ -26,6 +26,10 @@ export default {
         return value.match(/(h1|h2|h3|h4|h5|h6)/)
       },
     },
+    marginBottom: {
+      type: Boolean,
+      default: false,
+    },
   },
 }
 </script>
@@ -36,15 +40,23 @@ export default {
   margin-bottom: $space-xs;
   font-family: $font-heading;
 }
+
+.margin__bottom {
+  margin-bottom: $space-m;
+  @media #{$media-query-m} {
+    margin-bottom: $space-xl;
+  }
+}
+
 h1 {
   letter-spacing: $spacing-xs;
   font-size: $size-2xl;
-  line-height: $line-height-xl;
+  line-height: $line-height-l;
   margin-bottom: $space-s;
   font-weight: $weight-bold;
   @media #{$media-query-m} {
     font-size: $size-3xl;
-    line-height: $line-height-l;
+    line-height: $line-height-xl;
   }
 }
 h2 {
@@ -69,6 +81,7 @@ h4 {
   ```jsx
   <div>
     <Heading>The quick brown fox</Heading>
+    <Heading level="h2" marginBottom>The quick brown fox</Heading>
     <Heading level="h2">The quick brown fox</Heading>
     <Heading level="h3">The quick brown fox</Heading>
     <Heading level="h4">The quick brown fox</Heading>
